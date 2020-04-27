4290b1d5feb44283eeb4657efdd57c47cb9ea2aa
Anto Saha <ctanto333@gmail.com>
2020-04-27 08:37:49 +0300

    Add project files.

---
+++ AntoCAD/Form_4_W_O_BUS_COUPLER.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AntoCAD
+{
+    /// <summary>
+    /// Interaction logic for Form_4_W_O_BUS_COUPLER.xaml
+    /// </summary>
+    public partial class Form_4_W_O_BUS_COUPLER : Page
+    {
+        public Form_4_W_O_BUS_COUPLER()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ AntoCAD/Home.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AntoCAD
+{
+    /// <summary>
+    /// Interaction logic for Home.xaml
+    /// </summary>
+    public partial class Home : Page
+    {
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        private void Form_4_W_O_BUS_COUPLER_Click(object sender, RoutedEventArgs e)
+        {
+            Uri uri = new Uri("\\Form_4_W_O_BUS_COUPLER.xaml", UriKind.Relative);
+            this.NavigationService.Navigate(uri);
+        }
+    }
+}
